f7ec63615c1bed6977f12972f207cc1d8889e161
Kowkodivka <theeugenet@gmail.com>
2024-12-24 23:27:10 +0700

    Gas

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_pancam::{PanCam, PanCamPlugin};
 
 const MASS: f32 = 1.0;
-const SMOOTHING_RADIUS: f32 = 10.0;
-const TARGET_DENSITY: f32 = 5.0;
-const PRESSURE_MULTIPLIER: f32 = 1.0;
+const SMOOTHING_RADIUS: f32 = 1.5;
+const TARGET_DENSITY: f32 = 1000.0;
+const PRESSURE_MULTIPLIER: f32 = 2000.0;
+const WIDTH: f32 = 100.0;
+const HEIGHT: f32 = 100.0;
+const GRAVITY: f32 = 9.8;
 
 #[derive(Component)]
 struct Velocity(Vec3);
@@ -18,7 +21,10 @@ fn main() {
         .add_plugins(WorldInspectorPlugin::new())
         .add_plugins(PanCamPlugin::default())
         .add_systems(Startup, setup)
-        .add_systems(Update, simulation_step)
+        .add_systems(
+            Update,
+            (velocity_system, update_system, boundary_collision_system),
+        )
         .run();
 }
 
@@ -30,7 +36,7 @@ fn setup(
     commands.spawn((Camera2d, PanCam::default()));
 
     let square_size = 10;
-    let spacing = 10.0;
+    let spacing = 1.0;
 
     for x in 0..square_size {
         for y in 0..square_size {
@@ -97,6 +103,44 @@ fn calculate_pressure_force(point: Vec3, transforms: &[&Transform]) -> Vec3 {
         })
 }
 
-fn simulation_step(time: Res<Time>) {
+fn velocity_system(
+    time: Res<Time>,
+    transforms_query: Query<&Transform>,
+    mut velocities_query: Query<(&Transform, &mut Velocity)>,
+) {
     let delta_time = time.delta_secs();
+
+    let all_transforms: Vec<_> = transforms_query.iter().collect();
+
+    for (transform, mut velocity) in velocities_query.iter_mut() {
+        let position = transform.translation;
+        let pressure_force = calculate_pressure_force(position, &all_transforms);
+
+        velocity.0 += pressure_force * delta_time;
+        velocity.0 += Vec3::new(0.0, -1.0, 0.0) * GRAVITY * delta_time
+    }
+}
+
+fn update_system(time: Res<Time>, mut query: Query<(&mut Transform, &Velocity)>) {
+    let delta_time = time.delta_secs();
+
+    for (mut transform, velocity) in query.iter_mut() {
+        transform.translation += velocity.0 * delta_time;
+    }
+}
+
+fn boundary_collision_system(mut query: Query<(&mut Transform, &mut Velocity)>) {
+    for (mut transform, mut velocity) in query.iter_mut() {
+        let position = transform.translation;
+
+        if position.x <= -WIDTH / 2.0 || position.x >= WIDTH / 2.0 {
+            velocity.0.x *= -1.0;
+            transform.translation.x = position.x.clamp(-WIDTH / 2.0, WIDTH / 2.0);
+        }
+
+        if position.y <= -HEIGHT / 2.0 || position.y >= HEIGHT / 2.0 {
+            velocity.0.y *= -1.0;
+            transform.translation.y = position.y.clamp(-HEIGHT / 2.0, HEIGHT / 2.0);
+        }
+    }
 }
